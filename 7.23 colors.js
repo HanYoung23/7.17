340a2d6a8c0559feecac3c24e45e8b61412ed2bf
HanYoung23 <67942048+HanYoung23@users.noreply.github.com>
2020-07-23 22:19:33 +0900

    Add files via upload

---
+++ 7.23 colors.js
@@ -0,0 +1,34 @@
+var Links = {
+  setColor: function(color){
+    // var list = document.querySelectorAll('a');
+    //  var i = 0;
+    //  while(i < list.length){
+    //      list[i].style.color = color;
+    //      i = i + 1;}
+    $('a').css('color', color);
+  }
+}
+var Body={
+  setColor:function(color){
+  // document.querySelector('body').style.color = color;
+  $('body').css('color', color);
+},
+  setBackgroundColor:function(color){
+  // document.querySelector('body').style.backgroundColor = color;
+  $('body').css('backgroundColor', color);
+  }
+}
+function nightDayHandler(self){
+    var target = document.querySelector('body');
+    if(self.value === 'night'){
+      Body.setBackgroundColor('black');
+      Body.setColor('lime');
+      Links.setColor('red');
+      self.value = 'day';
+    } else {
+      Body.setBackgroundColor('white');
+      Body.setColor('black');
+      Links.setColor('orange');
+      self.value = 'night';
+  }
+}
